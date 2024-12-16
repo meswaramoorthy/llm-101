fa498feb322157faa94bfec89d86ad98839292aa
Maharajan Eswaramoorthy <41995288+meswaramoorthy@users.noreply.github.com>
2024-12-16 13:26:07 +0000

    qna basic webapp using streamlit

--- QnA/app_qna.py
+++ QnA/app_qna.py
@@ -2,7 +2,8 @@ import streamlit as st
 import getpass
 import os
 import bs4
-from langchain_openai import AzureChatOpenAI, OpenAIEmbeddings
+import json
+from langchain_openai import AzureChatOpenAI, OpenAIEmbeddings, AzureOpenAIEmbeddings
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_core.tools import tool
 from langchain import hub
@@ -19,23 +20,100 @@ from langgraph.prebuilt import ToolNode, tools_condition
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
 
-st.title("🦜🔗 Quickstart QnA App")
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
 
-llm = AzureChatOpenAI(
-    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-    azure_deployment="gpt-4o-mini",
-    openai_api_version="2024-02-15-preview",
-)
-
-def generate_response(input_text):
-    st.info(llm.invoke(input_text))
-
-with st.form("my_form"):
-    text = st.text_area(
-        "Enter text:",
-        "What are the three key pieces of advice for learning how to code?",
+# Initialize LLM
+@st.cache_resource
+def initialize_llm():
+    llm = AzureChatOpenAI(
+        azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+        azure_deployment="gpt-4o-mini",
+        openai_api_version="2024-02-15-preview",
     )
-    submitted = st.form_submit_button("Submit")
+    st.session_state["llm"] = llm
+    return llm
 
-if submitted:
-        generate_response(text)
+@st.cache_resource
+def load_docs_and_create_store():
+    with open('article_links.json', 'r') as f:
+        article_links = json.load(f)
+    # Load and chunk contents of the blog
+    loader = WebBaseLoader(
+        web_paths=(list(article_links.values())),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(class_=("post-content", "post-title", "post-header"))
+        ),
+    )
+    docs = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    all_splits = text_splitter.split_documents(docs)
+    print(f"Split blog post into {len(all_splits)} sub-documents.")
+
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    vector_store = InMemoryVectorStore(embeddings)
+
+    # Index chunks
+    _ = vector_store.add_documents(documents=all_splits)
+    st.session_state["vector_store"] = vector_store
+    return vector_store
+
+# Define application steps
+def retrieve(state: State):
+    if "vector_store" in st.session_state:
+        vector_store = st.session_state["vector_store"]
+    else:
+         raise Exception("Vector Store not found")
+    
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    
+    # Define prompt for question-answering
+    prompt = hub.pull("rlm/rag-prompt")
+    if "llm" in st.session_state:
+        llm = st.session_state["llm"]
+    else:
+         raise Exception("llm not found")
+
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+def getGraph():
+    if "graph" not in st.session_state:
+        graph_builder = StateGraph(State)
+        # Compile application and test
+        graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+        graph_builder.add_edge(START, "retrieve")
+        graph = graph_builder.compile()
+        st.session_state["graph"] = graph
+
+        response = graph.invoke({"question": "What is Task Decomposition?"})
+        print(response["answer"])
+    return st.session_state["graph"]
+
+def main():
+    st.title("Q&A Bot Powered by LLM")
+    
+    initialize_llm()
+    load_docs_and_create_store()
+    graph = getGraph()
+
+    with st.form("my_form"):
+        text = st.text_area(
+            "Enter text:",
+        )
+        submitted = st.form_submit_button("Submit")
+     
+    if submitted:
+        response = graph.invoke({"question": text})
+        st.info(response["answer"])
+
+if __name__ == "__main__":
+    main()
